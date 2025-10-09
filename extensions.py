0d12bf08f2483d5e8b51c32dfb2633c548bad510
lucas nunes <nsa.lucas0@gmail.com>
2025-10-09 17:10:05 -0300

    funcao para acesso sem estar autenticado

--- extensions.py
+++ extensions.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from flask_cors import CORS
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
@@ -8,3 +9,9 @@ login_manager.login_view = 'login'
 db = SQLAlchemy()
 cors = CORS()
 migrate = Migrate()
+
+
+# TRATATIVA CASO TENTE ACESSAR ROTA SEM ESTAR AUTENTICADO
+@login_manager.unauthorized_handler
+def unauthorized_access():
+  return jsonify({'message': 'Access denied, authentication required'})
